41ebddd5915f699f114e1231ae2f25dc8fc8d80c
PivaRos <danielgavgurbin@gmail.com>
2023-04-27 00:37:16 +0300

    fixed fucking stupid referance bug !!!!

--- buyer/components/product/options/optionProductTab.tsx
+++ buyer/components/product/options/optionProductTab.tsx
@@ -7,31 +7,30 @@ import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 
 interface Props {
-    optionProduct:optionProduct;
-    isChecked:boolean;
-    optionProductUnits:number[];
-    index:number;
-    setOptionProductUnits:React.Dispatch<React.SetStateAction<number[]>>
-    setOptionProductCheckedState:React.Dispatch<React.SetStateAction<boolean[]>>;
-    optionProductCheckedState:boolean[];
+    optionProduct: optionProduct;
+    isChecked: boolean;
+    optionProductUnits: number[];
+    index: number;
+    setOptionProductUnits: React.Dispatch<React.SetStateAction<number[]>>
+    setOptionProductCheckedState: React.Dispatch<React.SetStateAction<boolean[]>>;
+    optionProductCheckedState: boolean[];
 }
 
 
 const OptionProductTab = (props: Props) => {
     const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
     const [isChecked, setIsChecked] = useState(props.isChecked);
-    const [units, setUnits ] = useState(props.optionProductUnits[props.index]);
+    const [units, setUnits] = useState(props.optionProductUnits[props.index]);
 
-    const CheckBoxPressed = (isChecked:boolean) => {
+    const CheckBoxPressed = (isChecked: boolean) => {
         setIsChecked(isChecked);
         setChecked(isChecked);
     }
 
-    const setChecked = (isChecked:boolean) => {
+    const setChecked = (isChecked: boolean) => {
         let ToptionProductCheckedState = props.optionProductCheckedState;
         ToptionProductCheckedState[props.index] = isChecked;
-        if (isChecked === false)
-        {
+        if (isChecked === false) {
             setUnits(0);
             props.setOptionProductUnits(realoptionProductUnits => {
                 let Tunits = realoptionProductUnits
@@ -39,7 +38,7 @@ const OptionProductTab = (props: Props) => {
                 return Tunits;
             });
         }
-        else{
+        else {
             setUnits(1);
             props.setOptionProductUnits(realoptionProductUnits => {
                 console.log(realoptionProductUnits);
@@ -52,29 +51,30 @@ const OptionProductTab = (props: Props) => {
     }
 
     const changeUnitsUp = async () => {
-        setUnits(realunits => {return realunits+1});
+        setUnits(realunits => { return realunits + 1 });
         props.setOptionProductUnits(realoptionProductUnits => {
-            let Tunits = realoptionProductUnits
-            Tunits[props.index] = units;
+            let Tunits = JSON.parse(JSON.stringify(realoptionProductUnits));
+            Tunits[props.index] = Tunits[props.index] + 1;
             return Tunits;
+
         });
     }
 
     const changeUnitsDown = async () => {
-        if (units > 0){
+        if (units > 0) {
 
-            if (units-1 === 0){
+            if (units - 1 === 0) {
             }
-            else
-            {
+            else {
                 setUnits(currentValue => {
-                    return currentValue-1;
+                    return currentValue - 1;
                 });
                 let Tunits = props.optionProductUnits;
-                Tunits[props.index] = units-1;
+                Tunits[props.index] = units - 1;
                 props.setOptionProductUnits(value => {
                     let Tunits = value
                     Tunits[props.index] = units;
+                    console.log(Tunits);
                     return Tunits;
                 });
 
@@ -82,53 +82,53 @@ const OptionProductTab = (props: Props) => {
         }
     }
 
-    const comp = <View style={{marginLeft:6, alignContent:'center', flexDirection:'row', width:'100%'}}>
-        <Text style={{textAlign:'center', fontSize:17}}>{props.optionProduct.name}</Text>
-    {units > 0 && 
-        <View style={styles.PressableUnits}>
-        <Pressable style={styles.pressableButton} onPress={changeUnitsUp}>
-                <Text style={styles.buttonText}>+</Text>
-        </Pressable>
-        <Text style={[styles.unitText]}>{units}</Text>
-        <Pressable style={styles.pressableButton} onPress={changeUnitsDown} >
-                <Text style={styles.buttonText}>-</Text>
-        </Pressable>
-        </View>
-    }</View>;
+    const comp = <View style={{ marginLeft: 6, alignContent: 'center', flexDirection: 'row', width: '100%' }}>
+        <Text style={{ textAlign: 'center', fontSize: 17 }}>{props.optionProduct.name}</Text>
+        {units > 0 &&
+            <View style={styles.PressableUnits}>
+                <Pressable style={styles.pressableButton} onPress={changeUnitsUp}>
+                    <Text style={styles.buttonText}>+</Text>
+                </Pressable>
+                <Text style={[styles.unitText]}>{units}</Text>
+                <Pressable style={styles.pressableButton} onPress={changeUnitsDown} >
+                    <Text style={styles.buttonText}>-</Text>
+                </Pressable>
+            </View>
+        }</View>;
 
     return (
-        <View style={{padding:10, marginLeft:5}}>
-        <BouncyCheckbox 
-        isChecked={isChecked}
-        fillColor="lightgreen"
-        textComponent={comp}
-        onPress={(isChecked) => CheckBoxPressed(isChecked)}
-        /></View>);
+        <View style={{ padding: 10, marginLeft: 5 }}>
+            <BouncyCheckbox
+                isChecked={isChecked}
+                fillColor="lightgreen"
+                textComponent={comp}
+                onPress={(isChecked) => CheckBoxPressed(isChecked)}
+            /></View>);
 
 }
 
 const styles = StyleSheet.create({
 
-    pressableButton:{
-        zIndex:3,
-        padding:10,    
+    pressableButton: {
+        zIndex: 3,
+        padding: 10,
     },
-    PressableUnits:{
-        right:50,
-        width:70,
-        flexDirection:'row',
-        position:'absolute',
-        borderWidth:1,
-        borderColor:'black',
+    PressableUnits: {
+        right: 50,
+        width: 70,
+        flexDirection: 'row',
+        position: 'absolute',
+        borderWidth: 1,
+        borderColor: 'black',
     },
-    unitText:{
-        fontSize:16,
-          textAlign:'center',
-        marginBottom:10,
-        marginTop:10,
+    unitText: {
+        fontSize: 16,
+        textAlign: 'center',
+        marginBottom: 10,
+        marginTop: 10,
     },
-    buttonText:{
-        fontSize:16,
+    buttonText: {
+        fontSize: 16,
     },
 })
 
--- buyer/components/product/options/options_grid.tsx
+++ buyer/components/product/options/options_grid.tsx
@@ -11,25 +11,24 @@ import { GetOptionProduct } from "../../../functions";
 import { BackHandler } from "react-native";
 
 interface Props {
-    option:Option;
-    optionIndex:number;
-    store:Store;
-    setSelectedProduct:React.Dispatch<React.SetStateAction<Product | undefined>>;
-    selectedProduct:Product;
+    option: Option;
+    optionIndex: number;
+    store: Store;
+    setSelectedProduct: React.Dispatch<React.SetStateAction<Product | undefined>>;
+    selectedProduct: Product;
 }
 
 export const ProductOptionsList = (props: Props) => {
-    const [optionProductCheckedState, setOptionProductCheckedState] = useState<boolean[]>(props.option.optionProducts.map(() => {return false}));
+    const [optionProductCheckedState, setOptionProductCheckedState] = useState<boolean[]>(props.option.optionProducts.map(() => { return false }));
     const [optionProductUnits, setOptionProductUnits] = useState<number[]>(props.option.optionProducts.map(() => {
         return 0;
     }));
-    
-    const getTotalUnits = (array:number[]) => {
+
+    const getTotalUnits = (array: number[]) => {
         let TotalUnits = 0;
         array.forEach(units => {
-            if (units)
-            {
-                TotalUnits +=units;
+            if (units) {
+                TotalUnits += units;
             }
         });
         return TotalUnits;
@@ -37,59 +36,59 @@ export const ProductOptionsList = (props: Props) => {
 
     useEffect(() => {
         console.log(getTotalUnits(optionProductUnits));
-        if (props.option.selectedOptionProducts && props.option.selectedOptionProducts.length> 0) //if product is not default *
+        if (props.option.selectedOptionProducts && props.option.selectedOptionProducts.length > 0) //if product is not default *
         {
-        // need to update checkedState 
-        setOptionProductCheckedState(props.option.selectedOptionProducts.map((v) => {
-            return v.selected;
-        }))
-        // need to update units
-        setOptionProductUnits(props.option.selectedOptionProducts.map((v) => {
-            return v.units;
-        }))
+            // need to update checkedState 
+            setOptionProductCheckedState(props.option.selectedOptionProducts.map((v) => {
+                return v.selected;
+            }))
+            // need to update units
+            setOptionProductUnits(props.option.selectedOptionProducts.map((v) => {
+                return v.units;
+            }))
         }
     }, [])
+
 
     useEffect(() => {
         // not working properly 
         console.log("optionProducts Units Changed !");
-        console.log("the unmber array is"+optionProductUnits);
+        console.log("the unmber array is" + optionProductUnits);
         let option = props.option;
         option.selectedOptionProducts = option.optionProducts.map((v, index) => {
             return {
-                selected:optionProductCheckedState[index],
-                units:optionProductUnits[index]
+                selected: optionProductCheckedState[index],
+                units: optionProductUnits[index]
             };
         })
         let TselectedProduct = props.selectedProduct;
-        if (TselectedProduct.options)
-        {
+        if (TselectedProduct.options) {
             TselectedProduct.options[props.optionIndex] = option;
         }
         props.setSelectedProduct(TselectedProduct);
     }, [JSON.stringify(optionProductCheckedState), JSON.stringify(optionProductUnits)])
 
     return (<View style={styles.mainGrid}>
-        <Text style={{fontWeight:'bold', padding:5, paddingLeft:15, }}>{props.option.name}</Text>
-        {props.option.maxPicks > 1  && <Text style={{padding:2, color:"grey", paddingLeft:13, }}>ניתן לבחור עד {props.option.maxPicks} פריטים</Text>}
-        {props.option.maxPicks === 1  && <Text style={{padding:2, color:"grey", paddingLeft:13, }}>ניתן לבחור עד פריט אחד{getTotalUnits(optionProductUnits)>props.option.maxPicks && "+"}</Text>}
+        <Text style={{ fontWeight: 'bold', padding: 5, paddingLeft: 15, }}>{props.option.name}</Text>
+        {props.option.maxPicks > 1 && <Text style={{ padding: 2, color: "grey", paddingLeft: 13, }}>ניתן לבחור עד {props.option.maxPicks} פריטים</Text>}
+        {props.option.maxPicks === 1 && <Text style={{ padding: 2, color: "grey", paddingLeft: 13, }}>ניתן לבחור עד פריט אחד{getTotalUnits(optionProductUnits) > props.option.maxPicks && "+"}</Text>}
         {props.option.optionProducts.map((OptionProduct, index) => {
-           return <OptionProductTab optionProductCheckedState={optionProductCheckedState} optionProductUnits={optionProductUnits} setOptionProductUnits={setOptionProductUnits} isChecked={optionProductCheckedState[index]} setOptionProductCheckedState={setOptionProductCheckedState} key={index} index={index} optionProduct={GetOptionProduct(props.store ,OptionProduct)}/>
+            return <OptionProductTab optionProductCheckedState={optionProductCheckedState} optionProductUnits={optionProductUnits} setOptionProductUnits={setOptionProductUnits} isChecked={optionProductCheckedState[index]} setOptionProductCheckedState={setOptionProductCheckedState} key={index} index={index} optionProduct={GetOptionProduct(props.store, OptionProduct)} />
         })}
     </View>);
 }
 
 const styles = StyleSheet.create({
-    mainGrid:{
-        width:'100%',   
+    mainGrid: {
+        width: '100%',
     },
-    ChooseText:{
-        top:-7,
-        color:'grey',
-        left:5,
+    ChooseText: {
+        top: -7,
+        color: 'grey',
+        left: 5,
     },
-    optionName:{
-        padding:10,
-        fontWeight:'bold',
+    optionName: {
+        padding: 10,
+        fontWeight: 'bold',
     },
 })
