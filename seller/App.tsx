17bb8f9873e7e6e074f4907a990476428f5664fb
PivaRos <sbhtk478@gmail.com>
2023-05-08 20:18:25 +0300

    add login page with no logic

--- seller/App.tsx
+++ seller/App.tsx
@@ -1,17 +1,29 @@
 import { StyleSheet, Text, View, StatusBar, Platform, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ViewLogin } from './navigation/viewLogin';
+
+
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false, fullScreenGestureEnabled: true }}>
+          <Stack.Screen  name='ViewLogin' children={() => <ViewLogin/>}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     paddingTop:Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
