08ce4861f4541e9a6b6ce478814899034305560a
PivaRos <sbhtk478@gmail.com>
2023-05-09 23:19:55 +0300

    add login message and loading view

--- seller/navigation/viewLogin.tsx
+++ seller/navigation/viewLogin.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react"
-import { View, StyleSheet, Text, TextInput, ScrollView, Pressable, Animated } from "react-native"
+import { useEffect, useRef, useState } from "react"
+import { View, StyleSheet, Text, TextInput, ScrollView, Pressable, Animated, GestureResponderEvent, PermissionsAndroid } from "react-native"
 import { useKeyboard } from "../hooks/useKeyborad";
 import { uri } from "../envVars";
 import { DataObject } from "../interfaces";
@@ -13,9 +13,13 @@ export const ViewLogin = () => {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [message, setMessage ] = useState("");
+    const [loading, setLoading ] = useState(false);
 
 
-    const Login = async () => {
+    const Login = async (e:GestureResponderEvent) => {
+        setMessage("");
+        setLoading(true);
         try{
             const result = await fetch(uri+"/authorization/account", {
                 method:"POST",
@@ -37,19 +41,24 @@ export const ViewLogin = () => {
         }
         catch(e:any){
             //display error message
+            setMessage(e.message)
             console.log(e.message)
         }
+
+        setLoading(false);
     }
 
 
     return (
-        <ScrollView contentContainerStyle={{height:'100%', width:'100%', backgroundColor:'white'}} scrollEnabled={false} scrollEventThrottle={16} ref={(ref:ScrollView) => {scrollViewRef.current = ref}}>
+        <ScrollView keyboardShouldPersistTaps={'handled'} contentContainerStyle={{height:'100%', width:'100%', backgroundColor:'white'}} scrollEnabled={false} scrollEventThrottle={16} ref={(ref:ScrollView) => {scrollViewRef.current = ref}}>
             <View style={{
             justifyContent:'center',
             flexDirection:'row',
             height:'100%'
                     }}>
             <View style={styles.loginView}>
+                {loading && <Text style={[styles.message, {color:'black'}]}>Loading...</Text>}
+                <Text style={styles.message}>{message}</Text>
             <TextInput onChangeText={(v) => {
                 setUsername(v)
             }} placeholder="Username" style={styles.TextInputStyle}/>
@@ -58,7 +67,7 @@ export const ViewLogin = () => {
             }} placeholder="Password" secureTextEntry={true} style={styles.TextInputStyle}/>
             </View>
             
-            <Pressable onPress={() => Login()} style={{position:'absolute', bottom:keyboradHeight, backgroundColor:'lightgreen', width:'85%', borderRadius:10, height:50, justifyContent:'center', flexDirection:'row'}}><Text style={{marginTop:13, fontSize:19}}>Login to Store</Text></Pressable>
+            <Pressable onPress={(e) => Login(e)} style={{position:'absolute', bottom:keyboradHeight, backgroundColor:'lightgreen', width:'85%', borderRadius:10, height:50, justifyContent:'center', flexDirection:'row'}}><Text style={{marginTop:13, fontSize:19}}>Login to Store</Text></Pressable>
             </View>
         </ScrollView>
     )
@@ -66,6 +75,14 @@ export const ViewLogin = () => {
 
 
 const styles = StyleSheet.create({
+    message:{
+        color:'red',
+        textAlign:'center',
+        width:'100%',
+        padding:5,
+        marginTop:-10,
+        fontSize:16
+    },
     mainView:{
         backgroundColor:'red',
         height:'100%',
