630ef8da5416ec0cee52551d4c91bd578b58dcef
PivaRos <sbhtk478@gmail.com>
2023-05-11 01:37:38 +0300

    save

--- server/interfaces.ts
+++ server/interfaces.ts
@@ -81,6 +81,26 @@ export interface Store extends WithId<Document> {
   minOrder?:PriceObject
 }
 
+export const StoreChangeAble = {
+  _id: {admin:true, seller:false, support:false} as ChangePremonitions,
+  logo: {admin:true, seller:true, support:true} as ChangePremonitions,
+  name: {admin:true, seller:false, support:true} as ChangePremonitions,
+  products: {admin:true, seller:true, support:true} as ChangePremonitions,
+  authorizedUsers: {admin:true, seller:false, support:true} as ChangePremonitions,
+  location: {admin:true, seller:true, support:true} as ChangePremonitions,
+  deliveryDistance: {admin:true, seller:true, support:true} as ChangePremonitions,
+  openHoursObject: {admin:true, seller:true, support:true} as ChangePremonitions,
+  category: {admin:true, seller:false, support:true} as ChangePremonitions,
+  minOrder:{admin:true, seller:true, support:true} as ChangePremonitions
+}
+
+export interface ChangePremonitions
+{
+  admin:boolean,
+  support:boolean,
+  seller:boolean
+}
+
 export interface OpenHoursObject {
   openFrom: number
   closedFrom: number
@@ -137,3 +157,4 @@ export interface PaymentLog {
   priceCharged: number
   timestamp: number
 }
+
